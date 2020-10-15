4c20d3201e71e14498b3e69d71edf278d457628d
kaoryogata <34316046+kaoryogata@users.noreply.github.com>
2020-10-15 09:22:51 -0300

    Front Web - Home e Mapa completos
    - Mapbox
    - Routes
    - Layout: Home Mapa

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import Landing from './pages/Landing';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Landing} />
+                <Route path="/app" component={OrphanagesMap} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
